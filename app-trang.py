ee7ad3eac4193868f0aeedd608b98fc7e73d7050
Trang <trang.le@eduvaud.ch>
2025-05-10 17:52:24 +0200

    Première version de mon app Flask

---
+++ app-trang.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Ceci est un TP DevOps avec Flask, Git et Jenkins, réalisé par Trang."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
