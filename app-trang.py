173b0b82aee9c397e68bd4fd0153f3f6ac177eb8
Trang <trang.le@eduvaud.ch>
2025-05-10 20:55:24 +0200

    Changement de message d'accueil

--- app-trang.py
+++ app-trang.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Ceci est un TP DevOps avec Flask, Git et Jenkins, réalisé par Trang."
+    return "Ceci est un TP DevOps avec Flask, Git et Jenkins."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
