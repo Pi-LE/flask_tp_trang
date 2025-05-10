3b70cbcdc57286a7ba15f6a5186f534c5c37332a
Trang <trang.le@eduvaud.ch>
2025-05-10 23:22:25 +0200

    Validation CI/CD : nouveau message de bienvenue

--- app-trang.py
+++ app-trang.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Ceci est un TP DevOps avec Flask, Git et Jenkins."
+    return "Ceci est un TP DevOps ."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
